cc0c87c0932fe91ebb67189d6c9888490e1d0f5b
Dina Hafez <dinahafez.8@gmail.com>
2018-05-17 12:19:01 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,25 @@
+// function theBeatlesPlay(musicians, instruments) {
+//   var arr = [];
+//   for(var i = 0; i < musicians.length; i++) {
+//     arr.push(musicians[i]);
+//   }
+//   return arr;
+// }
+
+function johnLennonFacts(array) {
+  var i = 0;
+  while( i < array.length) {
+    array.push(array[i] +"!!!");
+    i++
+  }
+  return array;
+}
+
+function iLoveTheBeatles(n) {
+  var arr = [];
+  do {
+    arr.push("I love the Beatles!");
+    n++
+  } while(n < 15);
+  return arr;
+}
