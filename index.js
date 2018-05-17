cda1123bcfc72658e9273b6c0b9f93da381b2b53
Dina Hafez <dinahafez.8@gmail.com>
2018-05-17 12:29:32 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 function theBeatlesPlay(musicians, instruments) {
   var arr = [];
-  for(var i = 0; i < musicians.length; i++) {
-    arr.push(musicians[i]);
+  for(var i = 0; i < musicians.length, i < instruments.length ; i++) {
+    arr.push(musicians[i] + " plays " + );
   }
   return arr;
 }
